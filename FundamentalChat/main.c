865b27bcd2a211990c156358997cf1ecf0bf088a
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2019-12-16 00:02:42 +0330

    Initial Commit

---
+++ FundamentalChat/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FundamentalChat
+//
+//  Created by Soroush Jahanzad on 12/16/19.
+//  Copyright © 2019 Soroush Jahanzad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
