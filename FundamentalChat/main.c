7db551e0f2756cf281651503d0e21369b37892d3
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2019-12-24 14:06:04 +0330

    Main Structure Fixed and Refined

--- FundamentalChat/main.c
+++ FundamentalChat/main.c
@@ -7,82 +7,100 @@
 //
 
 #include <stdio.h>
-#include <sys/socket.h>
+#include <string.h>
 #include "cJSON.h"
+#include <sys/socket.h>
 
 // Constants --------------------------------------------------
 
 const int maxlen = 1024;
+const int port = 12345;
 
 // Types ------------------------------------------------------
 
 typedef char string[maxlen];
 
 enum boolean{False, True};
+enum states{account_menu_state, user_register_state, user_login_state, main_menu_state, create_channel_state, join_channel_state, user_logout_state, chat_menu_state, send_message_state, show_new_messages_state, show_channel_members_state, leave_channel_state};
 
 // Globals ----------------------------------------------------
 
 int state = 1;
 
+int sock;
+
 // Method prototypes
 
-void account_menu(void);            // State: 1
-void user_register(void);           // State: 2
-void user_login(void);              // State: 3
-void main_menu(void);               // State: 4
-void create_channel(void);          // State: 5
-void join_channel(void);            // State: 6
+void socket_init(void);
+int send_to_server(string);
+
+void account_menu(void);            // State: 0
+void user_register(void);           // State: 1
+void user_login(void);              // State: 2
+void main_menu(void);               // State: 3
+void create_channel(void);          // State: 4
+void join_channel(void);            // State: 5
+void user_logout(void);             // State: 6
 void chat_menu(void);               // State: 7
 void send_message(void);            // State: 8
 void show_new_messages(void);       // State: 9
 void show_channel_members(void);    // State: 10
+void leave_channel(void);           // State: 11
 
 // Main -------------------------------------------------------
 
 int main(int argc, const char * argv[]) {
-    while(state != 0)
+    while(state != -1)
     {
         switch (state) {
-            case 1:
+            case account_menu_state:
                 account_menu();
                 break;
             
-            case 2:
+            case user_register_state:
                 user_register();
                 break;
             
-            case 3:
+            case user_login_state:
                 user_login();
                 break;
             
-            case 4:
+            case main_menu_state:
                 main_menu();
                 break;
             
-            case 5:
+            case create_channel_state:
                 create_channel();
                 break;
             
-            case 6:
+            case join_channel_state:
                 join_channel();
                 break;
             
-            case 7:
+            case user_logout_state:
+                user_logout();
+                break;
+            
+            case chat_menu_state:
                 chat_menu();
                 break;
             
-            case 8:
+            case send_message_state:
                 send_message();
                 break;
             
-            case 9:
+            case show_new_messages_state:
                 show_new_messages();
                 break;
                 
-            case 10:
+            case show_channel_members_state:
                 show_channel_members();
                 break;
-                
+            
+            case leave_channel_state:
+                leave_channel();
+                break;
+            
             default:
                 break;
         }
@@ -94,7 +112,7 @@ int main(int argc, const char * argv[]) {
 
 // Methods ----------------------------------------------------
 
-void account_menu() // State: 1
+void account_menu() // State: 0
 {
     printf("Account menu\n");
     printf("1: Register\n");
@@ -106,15 +124,15 @@ void account_menu() // State: 1
         
     switch (request) {
         case 1:
-            state = 2;
+            state = user_register_state;
             break;
 
         case 2:
-            state = 3;
+            state = user_login_state;
             break;
             
         case 3:
-            state = 0;
+            state = -1;
             break;
                 
         default:
@@ -123,7 +141,7 @@ void account_menu() // State: 1
     }
 }
 
-void user_register() // State: 2
+void user_register() // State: 1
 {
     string username, password;
     
@@ -137,6 +155,8 @@ void user_register() // State: 2
     int success = 0;
     
     //TODO: Register
+    string request;
+    sprintf(request, "register %s, %s\n", username, password);
     
     if(success == 1) // Success
         printf("User successfully registered!\n");
@@ -145,10 +165,10 @@ void user_register() // State: 2
     else // ERROR
         printf("Oops! registration failed :(\n");
     
-    state = 1;
+    state = account_menu_state;
 }
 
-void user_login() // State: 3
+void user_login() // State: 2
 {
     string username, password;
     
@@ -163,18 +183,18 @@ void user_login() // State: 3
     //TODO: Login
     
     if(success == 1) // successful login
-        state = 4;
+        state = main_menu_state;
     else if(success == 2) // ERROR: no such user
     {
-        state = 1;
+        state = account_menu_state;
         printf("No such user exists!\n");
     } else { // ERROR: incorrect data
-        state = 1;
+        state = account_menu_state;
         printf("Incorrect username or password!\n");
     }
 }
 
-void main_menu() // State: 4
+void main_menu() // State: 3
 {
     printf("Main menu\n");
     printf("1: Create channel\n");
@@ -186,15 +206,15 @@ void main_menu() // State: 4
         
     switch (request) {
         case 1:
-            state = 5;
+            state = create_channel_state;
             break;
 
         case 2:
-            state = 6;
+            state = join_channel_state;
             break;
             
         case 3:
-            state = 1;
+            state = user_logout_state;
             break;
                 
         default:
@@ -203,7 +223,7 @@ void main_menu() // State: 4
     }
 }
 
-void create_channel() // State: 5
+void create_channel() // State: 4
 {
     string chname;
     printf("Please enter your channel name:\n");
@@ -215,15 +235,15 @@ void create_channel() // State: 5
     
     if(success) // Channel created successfully
     {
-        state = 7;
+        state = chat_menu_state;
         printf("Channel \"%s\" was successfully created!\n", chname);
     } else { // ERROR
-        state = 4;
+        state = main_menu_state;
         printf("Oops! An error occurred :(\n");
     }
 }
 
-void join_channel() // State: 6
+void join_channel() // State: 5
 {
     string chname;
     printf("Please enter the channel name:\n");
@@ -235,12 +255,19 @@ void join_channel() // State: 6
     
     if(success) // Joined channel successfully
     {
-        state = 7;
+        state = chat_menu_state;
         printf("You are now in the channel \"%s\"!\n", chname);
     } else { // ERROR
-        state = 4;
+        state = main_menu_state;
         printf("Oops! An error occurred :(\n");
     }
+}
+
+void user_logout() // State: 6
+{
+    //TODO: Logout
+    
+    state = account_menu_state;
 }
 
 void chat_menu() // State: 7
@@ -256,19 +283,19 @@ void chat_menu() // State: 7
         
     switch (request) {
         case 1:
-            state = 8;
+            state = send_message_state;
             break;
 
         case 2:
-            state = 9;
+            state = show_new_messages_state;
             break;
             
         case 3:
-            state = 10;
+            state = show_channel_members_state;
             break;
             
         case 4:
-            state = 4;
+            state = leave_channel_state;
             break;
                 
         default:
@@ -295,13 +322,17 @@ void send_message() // State: 8
     msg[i] = '\0';
     
     //TODO: send message
+    
+    state = chat_menu_state;
 }
 
 void show_new_messages() // State: 9
 {
-    printf("New messages:\n");
+    printf("New messages since last refresh:\n");
     
     //TODO: get and print messages
+    
+    state = chat_menu_state;
 }
 
 void show_channel_members() // State: 10
@@ -309,4 +340,27 @@ void show_channel_members() // State: 10
     printf("Channel members:\n");
     
     //TODO: get and print members
+    
+    state = chat_menu_state;
+}
+
+void leave_channel() // State: 11
+{
+    printf("Channel members:\n");
+    
+    //TODO: leave channel
+    
+    state = main_menu_state;
+}
+
+void socket_init()
+{
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+}
+
+int send_to_server(string message)
+{
+    //TODO: connect to server and send message
+    
+    return 0;
 }
